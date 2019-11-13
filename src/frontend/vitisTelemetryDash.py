948d235457d10b5868973c1656247c843d40417e
Christopher Yarp <cyarp@eecs.berkeley.edu>
2019-11-13 01:40:15 -0800

    Adding dummy callback for updating history plot.  Tweaking CSS to make plot look better.

--- src/frontend/vitisTelemetryDash.py
+++ src/frontend/vitisTelemetryDash.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
 import argparse
 import json
 
@@ -88,7 +89,7 @@ app.layout = html.Div(children=[
         html.H2(children = 'Historical Utilization:', id = 'hist-util'),
         html.Div(className = 'history-container', children = [
             dcc.Graph(
-                id='hist',
+                id='hist-plot',
                 animate = True,
                 config={
                     'showSendToCloud': False,
@@ -105,7 +106,7 @@ app.layout = html.Div(children=[
     html.Div(style={'display': 'none'}, children = [
         dcc.Interval(
             id='interval-component',
-            interval=1*1000, # in milliseconds
+            interval=10*1000, # in milliseconds
             n_intervals=0
         )
     ]),
@@ -116,18 +117,58 @@ app.layout = html.Div(children=[
 
 #Callbacks
 #Update the elements
-@app.callback(gaugeCallbackOutputs+[Output('refresh-ind', 'children')],
+@app.callback(gaugeCallbackOutputs+[Output('hist-plot', 'figure'), Output('refresh-ind', 'children')],
               [Input('interval-component', 'n_intervals')],
               [State('refresh-ind', 'children')])
 def interval_update(intervals, refresh_ind):
     current_ind = int(refresh_ind)
     new_ind = (current_ind+5)%101
 
+    #Gauge Updates
     rtnList = []
     for i in range(0, len(gaugeCallbackOutputs)):
         rtnList.append((new_ind+i)%101)
 
-    return tuple(rtnList) + tuple([str(new_ind)])
+    #History Updates
+    #Using Example from https://dash.plot.ly/getting-started-part-2
+    numPts = 10
+    x = range(0, numPts)
+    history_traces = []
+    for i in range(0, len(gaugeCallbackOutputs)):
+        initVal = (new_ind+i)%101
+        y = []
+        for j in range(0, numPts):
+            y.append(x[j]+initVal)
+
+        txt = 'Compute Partition ' + str(i)
+        history_traces.append(go.Scatter(
+                x=x,
+                y=y,
+                #text=[], #This is the label for each point
+                mode='lines+markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name=txt
+            ))
+    
+    new_fig = {
+        'data': history_traces,
+        'layout': dict(
+            xaxis={'type': 'linear', 'title': 'Time',
+                   'range':[x[0], x[numPts-1]]},
+            yaxis={'title': 'CPU Utilization', 'range': [0, 100]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition = {'duration': 500}
+        )
+    }
+
+    #Return everything
+    return tuple(rtnList) + tuple([new_fig]) + tuple([str(new_ind)]) #Array in tuple required to prevent string or dict from being broken apart
 
 if __name__ == '__main__':
     app.run_server(debug=True)
